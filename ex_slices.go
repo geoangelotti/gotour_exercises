3441180e19afb4bc6b018ceb3448e0587a456db1
Georgios Angelopoulos <georgios.angelopoulos@ibm.com>
2020-10-29 21:42:50 +0100

    slices

---
+++ ex_slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	var shell [][]uint8
+	for y := 0; y <= dy; y++ {
+		var core []uint8
+		for x := 0; x <= dx; x++ {
+			core = append(core, uint8(x^y))
+		}
+		shell = append(shell, core)
+	}
+	return shell
+}
+
+func main() {
+	pic.Show(Pic)
+}
